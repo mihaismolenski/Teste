a8cedde4840916d7224e107e086b41f66e5be9b3
mihaismolenski <mihaismolenski@gmail.com>
2015-05-23 15:25:16 +0300

    tot

---
+++ Tema3Sem2/Tema3Sem2/Controllers/InsertController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tema3Sem2.Controllers
+{
+    public class InsertController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+            Console.WriteLine(Request);
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Submit()
+        {
+            Console.WriteLine(Request);
+            return View("~/Views/Insert/Index.cshtml"); 
+        }
+    }
+}
