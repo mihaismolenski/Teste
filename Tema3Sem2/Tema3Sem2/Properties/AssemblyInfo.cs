e07a8fdcdf1ada9bf02ed38f400ba5059210eb05
mihaismolenski <mihaismolenski@gmail.com>
2015-05-23 09:45:41 +0300

    nou

--- Tema3Sem2/Tema3Sem2/Properties/AssemblyInfo.cs
+++ Tema3Sem2/Tema3Sem2/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("ec66a284-3411-4ac8-be23-1a2d24541c6d")]
+[assembly: Guid("ca06d1a5-1e7e-404b-9a9c-1799edd40bc4")]
 
 // Version information for an assembly consists of the following four values:
 //
