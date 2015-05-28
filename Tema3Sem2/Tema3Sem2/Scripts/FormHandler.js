64e1096031247c6beb25eee89c170b9acdda8d4f
Lulubul <didas_convict@yahoo.com>
2015-05-28 20:23:15 +0300

    FormHandler

---
+++ Tema3Sem2/Tema3Sem2/Scripts/FormHandler.js
@@ -0,0 +1,15 @@
+﻿
+function questionSubmit(item, questionId) {
+    
+    var formId = "#question_" + questionId;
+    console.log($(formId).serialize());
+    console.log(formId);
+    $.ajax({
+        url: '/Quiz/Submit',
+        type: 'POST',
+        data: $(formId).serialize(),
+        success: function (result) {
+            alert(result);
+        }
+    });
+}
