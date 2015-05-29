1a69cc48d3851218c57356b7f562499554a62191
alexandrurusu93 <alexandrurusu93@yahoo.com>
2015-05-29 21:52:15 +0300

    Rusu

--- Tema3Sem2/Tema3Sem2/Controllers/InsertController.cs
+++ Tema3Sem2/Tema3Sem2/Controllers/InsertController.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Tema3Sem2.Repositories;
+using Tema3Sem2.Models;
+using Tema3Sem2.Models.DB;
 
 namespace Tema3Sem2.Controllers
 {
@@ -15,11 +18,72 @@ namespace Tema3Sem2.Controllers
             return View();
         }
 
+        [System.Web.Services.WebMethod]
+        public int AddDomain(string domain)
+        {
+            //int id = DomeniuHelper.AdaugaDomeniu(domain, "");
+            int id = 1;
+            return id;
+        }
+
+        [System.Web.Services.WebMethod]
+        public int AddSubDomain(string id, string subdomain)
+        {
+            //int idSub = SubdomeniuHelper.AdaugaSubomeniu(id, subdomain, "");
+            int idSub = 1;
+            return idSub;
+        }
+
         [HttpPost]
         public ActionResult Submit()
         {
-            Console.WriteLine(Request);
+
+            //IntrebariHelper.AdaugaIntrebare();
+            Console.WriteLine(Request.Form);
+           
+            var ceva = Request.Form.GetValues(0);
+            int idDomeniu = Convert.ToInt32(ceva[0]);
+            
+            ceva = Request.Form.GetValues(2);
+            int idSubdomeniu = Convert.ToInt32(ceva[0]);
+
+            var titlu = Request.Form.GetValues(4)[0];
+
+            List<VariantaRaspun> variante = new List<VariantaRaspun>();
+
+            int nrRaspunsuri = 0;
+            for (int i = 5; i < Request.Form.AllKeys.Length - 1; i++)
+            {
+                VariantaRaspun varianta = new VariantaRaspun();
+                if (Request.Form.Keys[i].Contains("question") == true)
+                {
+                    //int id = Convert.ToInt32(Request.Form.Keys[i].Substring(8, Request.Form.GetValues(i)[0].Length));
+                    varianta.Text = Request.Form.GetValues(i)[0];
+                    if (Request.Form.Keys[i + 1].Contains("answer") == true)
+                    {
+                        varianta.Corect = true;
+                        nrRaspunsuri++;
+                        i++;
+                    }
+                    else
+                    {
+                        varianta.Corect = false;
+                    }
+
+                }
+                variante.Add(varianta);
+            }
+            int tip = 1;
+            if (nrRaspunsuri > 1)
+            {
+                tip = 2;
+            }
+
+            
+            //IntrebariHelper.AdaugaIntrebare(idDomeniu, idSubdomeniu, titlu, tip, variante);
+            
             return View("~/Views/Insert/Index.cshtml"); 
         }
+
     }
 }
--- Tema3Sem2/Tema3Sem2/Scripts/questions.js
+++ Tema3Sem2/Tema3Sem2/Scripts/questions.js
@@ -8,11 +8,70 @@
         e.preventDefault();
         if (x < max_fields) { //max input box allowed
             x++; //text box increment
-            $(wrapper).append('<div><input type="text" name="mytext[]"/><a href="#" class="remove_field">Remove</a></div>'); //add input box
+            $(wrapper).append('<div><input type="text" name="question' + x + '"/><label style="display:inline-block;"><input type="checkbox" name="answer' + x + '" />Correct? </label><a href="#" class="remove_field">Remove</a></div>'); //add input box
         }
     });
 
     $(wrapper).on("click", ".remove_field", function (e) { //user click on remove text
         e.preventDefault(); $(this).parent('div').remove(); x--;
-    })
+    });
+
+    $('select#domeniu').on('change', function () {
+        if ($(this).val() == "new") {
+            $('#domeniu_block').show();
+        }
+    });
+
+    $('#submit_domeniu').on('click', function (e) {
+        //ajax
+        e.preventDefault();
+        var domeniu = $('#new_domeniu').val();
+        if (domeniu) {
+            $.ajax({
+                type: "POST",
+                url: "/Insert/AddDomain",
+                data: '{domain: "' + domeniu + '" }',
+                success: function (response) {
+                    console.log(response);
+                    $('select#domeniu option').removeAttr('checked');
+                    $('select#domeniu').append('<option value="' + response + '" checked="checked">' + domeniu + '</option>');
+                    $('#domeniu_block').hide();
+                },
+                failure: function (response) {
+                    alert(response);
+                }
+            });
+        }
+    });
+
+    $('select#subdomeniu').on('change', function () {
+        if ($(this).val() == "new") {
+            $('#subdomeniu_block').show();
+        }
+    });
+
+    $('#submit_subdomeniu').on('click', function (e) {
+        //ajax
+        e.preventDefault();
+        var idDomeniu = $('select#domeniu').val();
+        var subdomeniu = $('#new_subdomeniu').val();
+        if (subdomeniu) {
+            $.ajax({
+                type: "POST",
+                url: "/Insert/AddSubDomain",
+                data: {id: idDomeniu , subdomain: subdomeniu},
+                success: function (response) {
+                    console.log(response);
+                    $('select#domeniu option').removeAttr('checked');
+                    $('select#subdomeniu').append('<option value="' + response + '" checked="checked">' + subdomeniu + '</option>');
+                    $('#subdomeniu_block').hide();
+                },
+                failure: function (response) {
+                    alert(response);
+                }
+            });
+        }
+    });
+
+
 });
